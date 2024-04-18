b64b3d2e83235e5a9adb4ab0f9d5878a530e07ac
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2024-04-18 17:38:53 +0200

    A new batch of exercise

--- Program.cs
+++ Program.cs
@@ -5,46 +5,184 @@
         static void Main(string[] args)
         {
             Console.WriteLine("--- Benvenuto ---");
-
-            Console.WriteLine("--- quanti elementi vuoi inserire nell'array? ---");
-            int arraylenght = Convert.ToInt32(Console.ReadLine()); 
-
-            int[] arrayTest = new int[arraylenght];
+            Console.WriteLine("-- scegli cosa vedere:\n - (1) : esercizio principale\n - (2) : esercizio extra bonus ");
+            int optionSelection = Convert.ToInt32(Console.ReadLine());
 
 
-            for( int j = 0;  j < arraylenght; j++ )
+
+            switch (optionSelection)
             {
-                Console.WriteLine($"inserisci dato della {j} cella:");
-                arrayTest[j] = Convert.ToInt32(Console.ReadLine()); ;
+                case (1):
+                    {
+
+                        Console.WriteLine("--- quanti elementi vuoi inserire nell'array? ---");
+                        int arraylenght = Convert.ToInt32(Console.ReadLine());
+
+                        int[] arrayTest = new int[arraylenght];
+
+
+                        for (int j = 0; j < arraylenght; j++)
+                        {
+                            Console.WriteLine($"inserisci dato della {j} cella:");
+                            arrayTest[j] = Convert.ToInt32(Console.ReadLine()); ;
+
+                        }
+
+
+
+                        int[] arrayTest2 = ElevaArrayAlQuadrato(arrayTest);
+
+
+                        Console.WriteLine($" -- L'array originale è ");
+                        StampaArray(arrayTest);
+                        Console.WriteLine($" -- L'array al quadrato è ");
+                        StampaArray(arrayTest2);
+
+
+                        Console.WriteLine($" -- verifica cambiamento array originale ");
+                        StampaArray(arrayTest);
+
+
+                        int sum = sommaElementiArray(arrayTest);
+                        Console.WriteLine($" -- La somma degi elementi nell'array base è {sum} ");
+                        double sumSquared = sommaElementiArray(arrayTest2);
+                        Console.WriteLine($" -- La somma degi elementi nell'array al quadrato è {sumSquared} ");
+
+
+
+
+
+                    }
+                    break;
+
+
+                /* da qui in poi la parte extra bonus 
+- Permette di Calcolare l'area di un cerchio  (I numeri sono in virgola quindi attenzione.)
+- Converta i gradi da Celsius a Farenheit 
+- Verificare se il numero fornito in input è un numero primo o no
+- Concatenare due stringhe date in ingresso (BONUS: Permettere all'utente anche di scegliere il carattere per la concatenazione)
+- Verificare se una parola data in input è palindroma (HELP: la funzione Equals delle stringhe ci può aiutare) */
+                case (2):
+                    {
+
+                        Console.WriteLine("quale esercizio vuoi provare ?");
+
+                        int option2 = Convert.ToInt32(Console.ReadLine());
+                        
+                        while(!(option2 >= 1) || !(option2 <=3  )) {
+                            Console.WriteLine("Non è un opzione valida, scegli un valore intero tra 1 e 3 compresi");
+                            option2 = Convert.ToInt32(Console.ReadLine());
+                        }
+
+                        switch (option2){
+
+                            case (1):
+                                {
+                                    Console.WriteLine("inserisci il raggio del cerchio");
+                                    decimal radiusTest = Convert.ToDecimal(Console.ReadLine());
+
+                                    decimal answer = (Math.Round(areaOfACrircle(radiusTest), 2));
+                                    Console.WriteLine($"{answer} è la risposta che cercavi");
+
+
+                                }
+                                break;
+
+                            case (2): {
+
+                                    Console.WriteLine("Inserisci ora i gradi celsius te li restituisco in farenheit (fino a 2 numeri dopo la virgola)");
+                                    decimal userCelsius = Convert.ToDecimal(Console.ReadLine());
+
+                                    decimal userFare = Math.Round(celToFar(userCelsius), 2);
+                                    Console.WriteLine($"{userFare} è la risposta che cercavi");
+                                }
+                                break;
+
+                            case (3):
+                                {
+                                    Console.WriteLine("inserisci un numero ti dico se è primo");
+
+                                    bool answer = isPrime(Convert.ToInt32(Console.ReadLine()));
+
+
+                                    if (answer == true)
+                                        Console.WriteLine("il numero è primo");
+                                    else if (answer == false)
+                                        Console.WriteLine("il numero non è primo");
+                                    else
+                                        Console.WriteLine("ops c'è un problema");
+                                }
+                                break;
+
+
+
+                                //da finire 
+
+
+                            case (4): {
+                                
+                                }
+                                break;
+
+
+                        }
+
+
+                        
+
+
+
+
+
+                        decimal areaOfACrircle(decimal radius)
+                        {
+                            decimal pi = Convert.ToDecimal(Math.PI);
+                            decimal newRadius = radius * radius;
+                            
+                            return (newRadius * pi);                         
+                        }
+
+
+                        decimal celToFar(decimal celsius)
+                        {
+                            decimal faren = ((celsius * 9 / 5) + 32);
+                            return faren;
+
+                        }
+
+
+                        bool isPrime(int number)
+                        {
+                           
+                            for (int i=2; i <= number/2; i++)
+                            {
+                                if (number % i == 0)
+                                    return false;
+                                    
+
+                            }
+
+                            return true;
+                        }
+
+
+                    }
+                    break;
+
             }
-
-
-
-            int[] arrayTest2 = ElevaArrayAlQuadrato(arrayTest);
-            Console.WriteLine($" -- L'array originale è ");
-            StampaArray(arrayTest);
-            Console.WriteLine($" -- L'array al quadrato è ");
-            StampaArray(arrayTest2);
-            Console.WriteLine($" -- verifica cambiamento array originale ");
-            StampaArray(arrayTest);
-            int sum = sommaElementiArray(arrayTest);
-            Console.WriteLine($" -- La somma degi elementi nell'array base è {sum} ");
-            double sumSquared = sommaElementiArray(arrayTest2);
-            Console.WriteLine($" -- La somma degi elementi nell'array al quadrato è {sumSquared} ");
-
 
 
             void StampaArray(int[] array)
             {
                 int i = 0;
-                foreach(int element in array)
+                foreach (int element in array)
                 {
-                    if(i == 0)
+                    if (i == 0)
                         Console.Write($"[ {element} -");
                     else if (i == array.Length - 1)
                         Console.Write($"- {element} ]\n");
-                    else 
-                        Console.Write($"- {element} -" );
+                    else
+                        Console.Write($"- {element} -");
                     i++;
                 }
 
@@ -61,7 +199,7 @@
             {
                 int[] arrayElevato = (int[])array.Clone();
 
-                for(int i =0; i<array.Length; i++)
+                for (int i = 0; i < array.Length; i++)
                 {
                     arrayElevato[i] = Quadrato(array[i]);
                 }
@@ -71,13 +209,13 @@
             }
 
 
-            
+
 
             int sommaElementiArray(int[] array)
             {
                 int sum = 0;
 
-                foreach  (int element in array)
+                foreach (int element in array)
                 {
                     sum += element;
                 }
@@ -86,14 +224,27 @@
 
 
             }
+
         }
-
-
-
     }
 
 
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
 /*In questo esercizio vi chiedo di definire qualche funzione di utilità che poi potete usare per poter fare operazioni complesse nei vostri programma principale.
 Scrivete nel vostro programma principale Program.cs le seguenti funzioni di base:
 -**void StampaArray(int[] array) * *: che preso un array di numeri interi, stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]”. Potete prendere quella fatta in classe questa mattina
@@ -107,4 +258,8 @@ Una volta completate queste funzioni di utilità di base, e dato il seguente arr
 - Stampare la somma di tutti i numeri elevati al quadrati
 **BONUS:**Convertire le funzioni appena dichiarate in funzioni generiche, ossia funzioni che possono lavorare con array di numeri interi **di lunghezza variabile**, ossia debbono poter funzionare sia che gli passi array di 5 elementi, sia di 6, di 7, di ... e così via.
 A questo punto modificare il programma in modo che chieda all’utente quanti numeri voglia inserire, e dopo di che questi vengono inseriti a mano dall’utente esternamente. Rieseguire il programma con l’input preso esternamente dall’utente.*/
+
+
+
+
 
